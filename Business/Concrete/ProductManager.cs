ebc11da36246f33b946ea6c45090127febbc105a
UtkuKaganYavuzdogannn <Utku_K_Y@outlook.com>
2025-05-24 14:25:14 +0300

    Enterprise business layers have been created.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,42 @@
+﻿using Business.Abstarct;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+
+    {
+
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+
+
+
+
+        public List<Product> GetAll()
+        {
+            // İş kodları vs.  
+
+
+
+            return _productDal.GetAll();
+
+        }
+    }
+
+
+
+
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,104 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+
+        List<Product> _products;
+
+
+
+        //Bir veritabanını simüle ettim List ile. Bunu constructorda oluşturdum.
+        public InMemoryProductDal()
+
+
+        {
+            _products = new List<Product> {
+
+
+                new Product{ProductId=1,CategoryId=1,ProductName="Bardak",UnitPrice=15,UnitsInStock=15},
+                new Product{ProductId=2,CategoryId=1,ProductName="Fincan",UnitPrice=500,UnitsInStock=3},
+                new Product{ProductId=3,CategoryId=2,ProductName="Tava",UnitPrice=1500,UnitsInStock=2},
+                new Product{ProductId=4,CategoryId=2,ProductName="Tencere",UnitPrice=150,UnitsInStock=65},
+                new Product{ProductId=5,CategoryId=2,ProductName="Masa",UnitPrice=85,UnitsInStock=1}
+
+
+            };
+        }
+
+
+
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+
+        // LINQ ile silmeliyiz . Silme işlemi biraz daha sorunlu çünkü yukarıdaki temsili datalar bir referans adresi ile tutuluyor.
+        // Nesne referansını bulup silmemiz lazım.
+        public void Delete(Product product)
+        {
+
+
+            Product productToDelete;  //Silmek istenen nesne için bir referans tutuyoruz.
+
+
+            //SingleOrDefault ile tek bir nesne döndürdük. _products listesi içinde product.ProductId ile eşleşen bir nesne arıyoruz.
+            // Bir nevi foreach ile dolaşmış olduk.
+            productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+
+            _products.Remove(productToDelete);
+
+        }
+
+
+
+
+
+        public void Update(Product product)
+        {
+
+            Product productToUpdate;
+
+
+            //Gönderilen ürünün ıd'sine sahip nesnenin referansını bul.
+
+            productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+
+            productToUpdate.ProductName = product.ProductName;
+
+            productToUpdate.CategoryId = product.CategoryId;
+
+            productToUpdate.UnitPrice = product.UnitPrice;
+
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+
+        }
+
+
+
+
+
+        //Where Sql'deki ile aynı çalışıyor şarta uyanları tekrar liste yapıp döndürüyor.
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+           return _products.Where(p=>p.CategoryId == categoryId).ToList();
+        }
+
+
+    }
+}
